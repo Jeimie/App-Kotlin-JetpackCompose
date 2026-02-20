ea9138cc7eedc07de100ca8284bcf61a44bf1b74
Jeimie Calderón <calderonjeimie@gmail.com>
2026-02-19 22:53:58 -0500

    modifiers en compose

--- app/src/main/java/com/jeimie/devices/DeviceItem.kt
+++ app/src/main/java/com/jeimie/devices/DeviceItem.kt
@@ -1,13 +1,17 @@
 package com.jeimie.devices
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jeimie.devices.ui.theme.DevicesTheme
 import com.jeimie.devices.ui.theme.Typography
@@ -16,11 +20,13 @@ import com.jeimie.devices.ui.theme.Typography
 fun DeviceView(device: Device){
     Row (verticalAlignment = Alignment.CenterVertically) {
         Icon(imageVector = Icons.Default.Phone,
-            contentDescription = null)
+            contentDescription = null,
+            modifier = Modifier.padding(horizontal= 16.dp))
         Column {
             Text(text = device.name, style = Typography.headlineMedium)
             Text(text = device.data?.color ?: "-", style = Typography.bodyMedium)
             Text(text = device.data?.capacity ?: "-", style = Typography.bodyMedium)
+            HorizontalDivider()
         }
     }
 }
--- app/src/main/java/com/jeimie/devices/MainScreen.kt
+++ app/src/main/java/com/jeimie/devices/MainScreen.kt
@@ -1,17 +1,23 @@
 package com.jeimie.devices
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jeimie.devices.ui.theme.DevicesTheme
+import com.jeimie.devices.ui.theme.Typography
 
 @Composable
 fun MainView(modifier: Modifier) {
     Text(text = "Comprar",
-        modifier = modifier)
+        modifier = modifier.fillMaxWidth(),
+    style = Typography.displayMedium,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
